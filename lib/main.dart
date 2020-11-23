6f12428940f41cf454c5556b4a0846455a707e02
urayusuke <crfnf690@yahoo.co.jp>
2020-11-23 23:29:25 +0900

    main update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:memo_app/memo.dart';
+// import 'package:memo_app/memo.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(home: MyApp()));
 }
 
@@ -18,10 +22,37 @@ class MyApp extends StatelessWidget {
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             Navigator.push(
-                context, MaterialPageRoute(builder: (context) => Memo()));
+                context, MaterialPageRoute(builder: (context) => TestList()));
           },
           child: Icon(Icons.add),
         ),
+      ),
+    );
+  }
+}
+
+class TestList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('test').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return new Text('Loading...');
+            default:
+              return new ListView(
+                children: snapshot.data.docs.map((DocumentSnapshot document) {
+                  return new ListTile(
+                    title: new Text(document['title']),
+                    subtitle: new Text(document['content']),
+                  );
+                }).toList(),
+              );
+          }
+        },
       ),
     );
   }
